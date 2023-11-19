97adfcd7f9a0e104377134cf1e9341cdd6535227
Sviatoslav Kovalev <kovalev.si@mipt.ru>
2023-11-19 23:23:41 +0300

    03_04 and 03_05 are done

--- 03_finite_state_machines/03_04_sqrt_formula_fsms/formula_1_impl_2_fsm.sv
+++ 03_finite_state_machines/03_04_sqrt_formula_fsms/formula_1_impl_2_fsm.sv
@@ -36,5 +36,89 @@ module formula_1_impl_2_fsm
     //
     // Design the FSM to calculate an answer and provide the correct `res` value
 
+    //------------------------------------------------------------------------
+    // States
+
+    enum logic [1:0]
+    {
+        st_idle        = 2'b00,
+        st_wait_ab_res = 2'b01,
+        st_wait_c_res  = 2'b10
+    }
+    state, next_state;
+
+    //------------------------------------------------------------------------
+    // Next state and isqrt interface
+
+    always_comb
+    begin
+        next_state  = state;
+
+        isqrt_1_x_vld = '0;
+        isqrt_1_x     = 'x;  // Don't care
+        isqrt_2_x_vld = '0;
+        isqrt_2_x     = 'x;  // Don't care
+
+        case (state)
+        st_idle:
+        begin
+            isqrt_1_x = a;
+            isqrt_2_x = b;
+
+            if (arg_vld)
+            begin
+                isqrt_1_x_vld = '1;
+                isqrt_2_x_vld = '1;
+                next_state  = st_wait_ab_res;
+            end
+        end
+
+        st_wait_ab_res:
+        begin
+            isqrt_1_x = c;
+            isqrt_2_x = 32'd0;
+
+            if (isqrt_1_y_vld & isqrt_2_y_vld)
+            begin
+                isqrt_1_x_vld = '1;
+                isqrt_2_x_vld = '1;
+                next_state  = st_wait_c_res;
+            end
+        end
+
+        st_wait_c_res:
+        begin
+            if (isqrt_1_y_vld & isqrt_2_y_vld)
+            begin
+                next_state = st_idle;
+            end
+        end
+        endcase
+    end
+
+    //------------------------------------------------------------------------
+    // Assigning next state
+
+    always_ff @ (posedge clk)
+        if (rst)
+            state <= st_idle;
+        else
+            state <= next_state;
+
+    //------------------------------------------------------------------------
+    // Accumulating the result
+
+    always_ff @ (posedge clk)
+        if (rst)
+            res_vld <= '0;
+        else
+            res_vld <= ((state == st_wait_c_res) & (isqrt_1_y_vld & isqrt_2_y_vld));
+
+    always_ff @ (posedge clk)
+        if (state == st_idle)
+            res <= '0;
+        else if (isqrt_1_y_vld & isqrt_2_y_vld)
+            res <= res + isqrt_1_y + isqrt_2_y;
+
 
 endmodule
--- 03_finite_state_machines/03_04_sqrt_formula_fsms/formula_2_fsm.sv
+++ 03_finite_state_machines/03_04_sqrt_formula_fsms/formula_2_fsm.sv
@@ -29,5 +29,93 @@ module formula_2_fsm
     //
     // Design the FSM to calculate answer step-by-step and provide the correct `res` value
 
+    //------------------------------------------------------------------------
+    // States
+
+    enum logic [1:0]
+    {
+        st_idle     = 2'b00,
+        st_wait_c   = 2'b01,
+        st_wait_cb  = 2'b10,
+        st_wait_cba = 2'b11
+    }
+    state, next_state;
+
+    //------------------------------------------------------------------------
+    // Next state and isqrt interface
+
+    always_comb
+    begin
+        next_state  = state;
+
+        isqrt_x_vld = '0;
+        isqrt_x     = 'x;  // Don't care
+
+        case (state)
+        st_idle:
+        begin
+            isqrt_x = c;
+
+            if (arg_vld)
+            begin
+                isqrt_x_vld = '1;
+                next_state  = st_wait_c;
+            end
+        end
+
+        st_wait_c:
+        begin
+            if (isqrt_y_vld)
+            begin
+                isqrt_x = b + isqrt_y;
+                isqrt_x_vld = '1;
+                next_state  = st_wait_cb;
+            end
+        end
+
+        st_wait_cb:
+        begin
+            if (isqrt_y_vld)
+            begin
+                isqrt_x = a + isqrt_y;
+                isqrt_x_vld = '1;
+                next_state  = st_wait_cba;
+            end
+        end
+
+        st_wait_cba:
+        begin
+            if (isqrt_y_vld)
+            begin
+                next_state = st_idle;
+            end
+        end
+        endcase
+    end
+
+    //------------------------------------------------------------------------
+    // Assigning next state
+
+    always_ff @ (posedge clk)
+        if (rst)
+            state <= st_idle;
+        else
+            state <= next_state;
+
+    //------------------------------------------------------------------------
+    // Accumulating the result
+
+    always_ff @ (posedge clk)
+        if (rst)
+            res_vld <= '0;
+        else
+            res_vld <= ((state == st_wait_cba) & isqrt_y_vld);
+
+    always_ff @ (posedge clk)
+        if (state == st_idle)
+            res <= '0;
+        else if (isqrt_y_vld)
+            res <= isqrt_y;
+
 
 endmodule
